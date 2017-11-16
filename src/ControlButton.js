193174699feba8bf63bd265b6083441d41458846
urushiyama <aswif10flis1ntkb@gmail.com>
2017-11-16 18:45:31 +0900

    Add operator buttons in view

--- src/ControlButton.js
+++ src/ControlButton.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import FlatButton from 'material-ui/FlatButton'
+import FlatButton from 'material-ui/FlatButton';
 import './ControlButton.css';
 
 class ControlButton extends Component {
   render() {
     return (
       <div className="ControlButton">
-        <FlatButton className="ControlButton-button" label={this.props.value} style={{width: '100%'}} onClick={() => this.props.onClick()}/>
+        <FlatButton
+          className="ControlButton-button"
+          label={this.props.value}
+          style={{width: '100%'}}
+          primary={this.props.primary}
+          secondary={this.props.secondary}
+          onClick={() => this.props.onClick()}
+        />
       </div>
     );
   }
--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -7,24 +7,34 @@ class ControlPanel extends Component {
     return (
       <div className="ControlPanel">
         <div className="ControlPanel-row">
-          <ControlButton value={'7'} onClick={() => this.props.onClick('7')} />
-          <ControlButton value={'8'} onClick={() => this.props.onClick('8')} />
-          <ControlButton value={'9'} onClick={() => this.props.onClick('9')} />
+          <ControlButton value={'C/CE'} onClick={() => this.props.onClick('C/CE')} primary={true} />
+          <ControlButton value={'+/-'} onClick={() => this.props.onClick('+/-')} secondary={true} />
+          <ControlButton value={'%'} onClick={() => this.props.onClick('%')} secondary={true} />
+          <ControlButton value={'÷'} onClick={() => this.props.onClick('/')} secondary={true} />
         </div>
         <div className="ControlPanel-row">
-          <ControlButton value={'4'} onClick={() => this.props.onClick('4')} />
-          <ControlButton value={'5'} onClick={() => this.props.onClick('5')} />
-          <ControlButton value={'6'} onClick={() => this.props.onClick('6')} />
+          <ControlButton value={'7'} onClick={() => this.props.onClick('7')}  />
+          <ControlButton value={'8'} onClick={() => this.props.onClick('8')}  />
+          <ControlButton value={'9'} onClick={() => this.props.onClick('9')}  />
+          <ControlButton value={'×'} onClick={() => this.props.onClick('*')} secondary={true} />
         </div>
         <div className="ControlPanel-row">
-          <ControlButton value={'1'} onClick={() => this.props.onClick('1')} />
-          <ControlButton value={'2'} onClick={() => this.props.onClick('2')}/>
-          <ControlButton value={'3'} onClick={() => this.props.onClick('3')}/>
+          <ControlButton value={'4'} onClick={() => this.props.onClick('4')}  />
+          <ControlButton value={'5'} onClick={() => this.props.onClick('5')}  />
+          <ControlButton value={'6'} onClick={() => this.props.onClick('6')}  />
+          <ControlButton value={'-'} onClick={() => this.props.onClick('-')} secondary={true} />
         </div>
         <div className="ControlPanel-row">
-          <ControlButton value={'00'} onClick={() => this.props.onClick('00')} />
-          <ControlButton value={'0'} onClick={() => this.props.onClick('0')} />
-          <ControlButton value={'.'} onClick={() => this.props.onClick('.')} />
+          <ControlButton value={'1'} onClick={() => this.props.onClick('1')}  />
+          <ControlButton value={'2'} onClick={() => this.props.onClick('2')}  />
+          <ControlButton value={'3'} onClick={() => this.props.onClick('3')}  />
+          <ControlButton value={'+'} onClick={() => this.props.onClick('+')} secondary={true} />
+        </div>
+        <div className="ControlPanel-row">
+          <ControlButton value={'00'} onClick={() => this.props.onClick('00')}  />
+          <ControlButton value={'0'} onClick={() => this.props.onClick('0')}  />
+          <ControlButton value={'.'} onClick={() => this.props.onClick('.')}  />
+          <ControlButton value={'='} onClick={() => this.props.onClick('=')} primary={true} />
         </div>
       </div>
     );
